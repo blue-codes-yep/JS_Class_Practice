0fa45c7a592b3e63e15d1e3b435d123035c75dfa
Blue Lindsey <linzblue5@gmail.com>
2020-02-25 13:11:04 -0500

    Re-wrote into class, and a timout on the greet.

--- Javascript_Objects_Exercises.js
+++ Javascript_Objects_Exercises.js
@@ -1,19 +1,44 @@
 
-const mom = {
-    first: 'Alice',
-    last: 'Wong',
-    eyeColor: 'brown',
-    hairColor: 'black'
-};
+// const mom = {
+//     first: 'Alice',
+//     last: 'Wong',
+//     eyeColor: 'brown',
+//     hairColor: 'black'
+// };
 
 
-let daughter = {
-    first: 'Ilene',
-    hairColor: 'brown'
-};
+// let daughter = {
+//     first: 'Ilene',
+//     hairColor: 'brown'
+// };
 
-daughter.__proto__ = mom;
+// daughter.__proto__ = mom;
 
-console.log(daughter.eyeColor, daughter.last)
+// console.log(daughter.eyeColor, daughter.last)
 
 
+
+class Person{
+    constructor(name) { 
+        this.name = name;
+        this.friends = [];
+    }
+    addfriend(friend) {
+        this.friends.push(friend);
+    }
+    createGreeting(other) {
+        return 'Yo ' + other.name + '! from ' + this.name + '.';
+    }
+    greet(other) {
+        console.log(this.createGreeting(other));
+    }
+    timeout(other) {
+        let me = this
+        setTimeout(() => me.greet(other), 2000);
+    }
+}
+
+const me = new Person("Paxton");
+const friend = new Person("Logan");
+
+me.timeout(friend)
