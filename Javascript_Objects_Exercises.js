b4c3fcf74d9fd7c703295e1b12528f8fe12b985a
Blue Lindsey <linzblue5@gmail.com>
2020-02-25 17:02:55 -0500

    Refactored Code

--- Javascript_Objects_Exercises.js
+++ Javascript_Objects_Exercises.js
@@ -1,4 +1,5 @@
-
+// Inheritance
+// -------------
 // const mom = {
 //     first: 'Alice',
 //     last: 'Wong',
@@ -16,10 +17,34 @@
 
 // console.log(daughter.eyeColor, daughter.last)
 
+// ----------------------------
+// Person
+// Given this Person type:
 
+// function Person(name) {
+//      this.name = name;
+//      this.friends = [];
+// }
 
-class Person{
-    constructor(name) { 
+// Person.prototype.addFriend = function(friend) {
+//      this.friends.push(friend);
+// };
+
+// Person.prototype.createGreeting = function(other) {
+//      return 'Yo ' + other.name + '! from ' + this.name + '.';
+// };
+
+// Person.prototype.greet = function(other) {
+//      console.log(this.createGreeting(other));
+// };
+// Rewrite the above type as a class.
+
+// These thises
+// Given the Person type given in the last problem, 
+//add to it a lazyGreet method which will print out the same greeting message but only after 2 seconds.
+
+class Person {
+    constructor(name) {
         this.name = name;
         this.friends = [];
     }
